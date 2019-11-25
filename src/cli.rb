997edf10cfa5f83f5e7eeefd7cbbdbb9c6ffe5c8
TimoSci <me@timosci.net>
2019-11-25 23:32:42 +0100

    score is being returned correctly

--- src/cli.rb
+++ src/cli.rb
@@ -7,21 +7,28 @@ DATA_LOCATION = './data.yml'
 
 class Logger
 
+  #TOD0 separate forecasts and current forecast into separate classes in an ORM style
+
   attr_reader :database
 
   def initialize(database=DATA_LOCATION)
     @database = YAML::Store.new(database)
-    # @database.transaction{ @database[:current_forecast] ||= []}
+    @database.transaction{ @database[:current_forecast] ||= nil}
     @database.transaction{ @database[:forecasts] ||= []}
   end
 
   def save(outcome)
     current_forecast = self.forecast
     database.transaction do
-       database[:forecasts] << {time: Time.now.utc, forecast: current_forecast, outcome: outcome}
+       database[:forecasts] << {time: Time.now.utc, probability: current_forecast, outcome: outcome}
     end
     self.forecast = nil
   end
+
+  def all_forecasts
+    database.transaction{ database[:forecasts] }
+  end
+
 
   def forecast=(value)  #TODO: store state in memory not disk (environment variable?)
     database.transaction{database[:current_forecast] = value}
@@ -66,8 +73,6 @@ class CLI
   include ScoreFunctions
 
   def initialize(logger = Logger.new)
-    # @state ||= :ready
-    # @state = logger.state
     @logger = logger
   end
 
@@ -98,7 +103,6 @@ class CLI
 
   def enter_forecast(probability)
     raise "Probability must be between 0 and 1" unless probability >=0 && probability <= 1
-    # current_probability = probability
     logger.forecast = probability
   end
 
@@ -111,12 +115,37 @@ class CLI
     end
   end
 
-  def submit_query(command)
-    pp command
+  def submit_query(query)
+    valid_queries = [:score,:trend,:reset]
+    raise "invalid query! \n Valid queries are #{valid_queries.inspect}" unless valid_queries.include? query
+    pp self.public_send(query)
   end
+
 
   def clear_data
     logger.clear_all
+  end
+
+
+  #queries
+
+  def score
+    forecasts = logger.all_forecasts
+    brier_scores = forecasts.map do |forecast|
+      single_brier_score(forecast[:probability],forecast[:outcome])
+    end
+    brier_score(brier_scores)
+  end
+
+  def trend
+    "<<<palceholder for printing trend>>>"
+  end
+
+  def reset
+    # puts "Reset datastore. Are you sure? (y/n)"
+    # if gets == 'y'
+      "reset"
+    # end
   end
 
 
